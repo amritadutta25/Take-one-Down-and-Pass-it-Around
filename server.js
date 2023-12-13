31017af7c5bd27b0e5e5e61c8ef4e6476ed992f5
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-13 15:01:37 -0500

    Updated route to start over when 0 bottles left to be taken down

--- server.js
+++ server.js
@@ -18,14 +18,23 @@ app.get("/", (req, res) => {
 //Route to "/:number_of_bottles"
 app.get("/:number_of_bottles", (req, res) => {
 
-    // getting the number of bottles left from URL param
+    // getting the number of bottles on the wall from URL param
     const bottleNum = req.params.number_of_bottles
 
-    res.send(`<h1>${bottleNum} Bottles of beer on the wall</h1>
-    <a href="./${bottleNum - 1}">take one down, pass it around</a>`)
+    // number of bottles left to be taken down
+    const oneLessBottle = bottleNum - 1
+
+    // case when bottles are left to be taken down
+    if (Number(bottleNum) !== 0) { // Number() converts bottleNum from string to int type, since URL params are of string type
+        res.send(`<h1>${bottleNum} Bottles of beer on the wall</h1>
+                 <a href="./${oneLessBottle}">take one down, pass it around</a>`)
+    }
+    // cade when 0 bottles left to be taken down, we start over
+    else {
+        res.send(`<h1>${bottleNum} Bottles of beer on the wall</h1>
+        <a href="./">Start over</a>`)
+    }
 })
-
-
 
 
 // app listener
