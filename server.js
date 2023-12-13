2ec693dfa1b93274ed23ee61a6407a4e6091c34f
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-13 14:46:57 -0500

    Added route to '/:number_of_bottles'

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.get("/", (req, res) => {
              <a href="./98">take one down, pass it around</a>`)
 })
 
+//Route to "/:number_of_bottles"
+app.get("/:number_of_bottles", (req, res) => {
 
+    // getting the number of bottles left from URL param
+    const bottleNum = req.params.number_of_bottles
+
+    res.send(`<h1>${bottleNum} Bottles of beer on the wall</h1>
+    <a href="./${bottleNum - 1}">take one down, pass it around</a>`)
+})
 
 
 
